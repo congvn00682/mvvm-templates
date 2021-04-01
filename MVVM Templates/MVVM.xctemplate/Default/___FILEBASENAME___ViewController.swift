9830b411b2f86f8ca2a7b4a3772c42ba76910b6b
Vu Ngoc Cong <congvn00682@gmail.com>
2021-04-01 16:53:13 +0700

    init templates

---
+++ MVVM Templates/MVVM.xctemplate/Default/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//  
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ congvn. All rights reserved.
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+
+
+  override func viewDidLoad() {
+     super.viewDidLoad()
+  }
+
+}
---
+++ MVVM Templates/MVVM.xctemplate/WithWorker/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//  
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ congvn. All rights reserved.
+//
+
+import Foundation
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+
+
+}
